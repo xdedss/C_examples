bdfe0d4318b153670f86ec2f34b41aaded0a477a
LZR <liuzhuoran_2000@163.com>
2019-03-11 22:59:34 +0800

    Update G.c

--- 20190308_2018级 航空航天类第一次练习赛/G.c
+++ 20190308_2018级 航空航天类第一次练习赛/G.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 #include <stdbool.h> 
 
-//����������Ҫ��������������Ȱѽ��յ���ֵ���������ٵ��������
-//����Ҫ�õ�����ṹ 
+//这题由于需要逆序输出，必须先把接收到的值存起来，再倒过来输出
+//所以要用到数组结构 
 int main(){
 	char c;
 	char index = 0;
 	char array[128];
-	while(~scanf("%c", &c)){                           //���ϵض�ȡ��һ���ַ�ֱ������ 
-		if((c<=90 && c>=65) || (c<=122 && c>=97)){     //�����һ����ĸ 
-			array[index++] = c;                        //���ӵ������� 
+	while(~scanf("%c", &c)){                           //不断地读取下一个字符直到结束 
+		if((c<=90 && c>=65) || (c<=122 && c>=97)){     //如果是一个字母 
+			array[index++] = c;                        //添加到数组里 
 		}
+		//注：如果想在控制台里测试这个程序，请在输入完成后再输入control+Z，这样才能结束循环（相当于输入EOF）
 	}
 	while(index){
-		printf("%c", array[--index]);       //����������� 
+		printf("%c", array[--index]);       //逆序输出数组 
 	}
 }
